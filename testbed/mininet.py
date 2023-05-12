6675e2bc6fb0622eca193d164b5930e9b72cceeb
AldrinMD <72550659+AldrinMD@users.noreply.github.com>
2023-05-12 02:46:23 +0100

    Update

--- testbed/mininet.py
+++ testbed/mininet.py
@@ -1,33 +1,19 @@
-#!/usr/bin/env python
-from mininet.net import Mininet
-from mininet.node import RemoteController
-from mininet.cli import CLI
-from mininet.log import setLogLevel, info
+from mininet.topo import Topo  
+class MyTopo( Topo ):  
+    def __init__( self ):
+        # Initialize topology
+        Topo.__init__( self )
 
-def Net():
-    net = Mininet( controller=RemoteController )
-    net.addController( 'c0', controller=RemoteController, ip="192.168.30.218",port=8181 )
+        # Adding hosts and switches
+        Host1 = self.addHost( 'h1' )
+        Host2 = self.addHost( 'h2' )
+        Switch1 = self.addSwitch('s1')
+        Switch2 = self.addSwitch('s2')
 
-    h1 = net.addHost( 'h1', ip='192.168.30.217/index.html' )
-    h2 = net.addHost( 'h2', ip='10.0.0.2' )
+        # Linking hosts and switches
+        self.addLink( Host1, Switch1 )
+        self.addLink( Switch1, Switch2 )
+        self.addLink( Switch2, Host2 )
+topos = { 'mytopo': ( lambda: MyTopo() ) }  
 
-    s1 = net.addSwitch( 's3')
-    s2 = net.addSwitch( 's3')
-
-    info( '*** Creating links\n' )
-    net.addLink( h1, s1 )
-    net.addLink( h2, s2 )
-    net.addLink( s1, s2 )
-
-    info( '*** Starting network\n')
-    net.start()
-
-    info( '*** Running CLI\n' )
-    CLI( net )
-
-    info( '*** Stopping network' )
-    net.stop()
-
-if __name__ == '__main__':
-    setLogLevel( 'info' )
-    Net()
+#sudo mn --custom mininet.py --controler=remote,ip=192.168.30.218,port=6633 --switch ovsk, protocols=OpenFlow13
