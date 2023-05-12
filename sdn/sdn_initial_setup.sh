b28135be78a2b9a482788e3120dac651914f707d
AldrinMD <72550659+AldrinMD@users.noreply.github.com>
2023-05-12 22:26:21 +0100

    Update

--- sdn/sdn_initial_setup.sh
+++ sdn/sdn_initial_setup.sh
@@ -14,8 +14,18 @@ else
 	sudo apt -y install openjdk-8-jdk
 fi
 
+# iperf Install Check
+dpkg -s iperf &> /dev/null
+if [ $? -eq 0 ]; then
+	echo "iperf  already installed"
+else
+	echo "Installing iperf"
+	sudo apt-get -y install iperf 
+fi
+
 echo "VM IP:"
 hostname -I | awk '{print $1}'
+wget https://nexus.opendaylight.org/content/repositories/public/org/opendaylight/integration/distribution-karaf/0.4.4-Beryllium-SR4/distribution-karaf-0.4.4-Beryllium-SR4.zip
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
 # feature:install odl-restconf odl-l2switch-switch odl-mdsal-apidocs odl-dlux-all odl-openflowplugin-all
--- testbed/mininet.py
+++ testbed/mininet.py
@@ -1,19 +1,21 @@
 from mininet.topo import Topo  
+from mininet.link import TCLink
+
 class MyTopo( Topo ):  
     def __init__( self ):
         # Initialize topology
         Topo.__init__( self )
 
         # Adding hosts and switches
-        Host1 = self.addHost( 'h1' )
-        Host2 = self.addHost( 'h2' )
+        Host1 = self.addHost( 'h1', ip='192.168.30.217' )
+        Host2 = self.addHost( 'h2', ip='192.168.30.218' )
         Switch1 = self.addSwitch('s1')
         Switch2 = self.addSwitch('s2')
 
         # Linking hosts and switches
         self.addLink( Host1, Switch1 )
-        self.addLink( Switch1, Switch2 )
+        self.addLink( Switch1, Switch2, cls=TCLink,bw=1 )
         self.addLink( Switch2, Host2 )
 topos = { 'mytopo': ( lambda: MyTopo() ) }  
 
-#sudo mn --custom mininet.py --controler=remote,ip=192.168.30.218,port=6633 --switch ovsk, protocols=OpenFlow13
+#sudo mn --custom mininet.py --topo mytopo --controller=remote,ip=192.168.30.218,port=6633 --switch ovsk,protocols=OpenFlow13
