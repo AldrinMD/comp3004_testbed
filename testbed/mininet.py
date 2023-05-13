d879867c42cd7fa85794bf9ff7f4cf573e080a00
AldrinMD <72550659+AldrinMD@users.noreply.github.com>
2023-05-13 13:32:50 +0100

    Update

--- testbed/mininet.py
+++ testbed/mininet.py
@@ -7,14 +7,14 @@ class MyTopo( Topo ):
         Topo.__init__( self )
 
         # Adding hosts and switches
-        Host1 = self.addHost( 'h1', ip='192.168.30.217' )
-        Host2 = self.addHost( 'h2', ip='192.168.30.218' )
+        Host1 = self.addHost( 'h1', ip='10.0.0.1' )
+        Host2 = self.addHost( 'h2', ip='10.0.0.2' )
         Switch1 = self.addSwitch('s1')
         Switch2 = self.addSwitch('s2')
 
         # Linking hosts and switches
         self.addLink( Host1, Switch1 )
-        self.addLink( Switch1, Switch2, cls=TCLink,bw=1 )
+        self.addLink( Switch1, Switch2, cls=TCLink,bw=500 ) # bw = bandwidth
         self.addLink( Switch2, Host2 )
 topos = { 'mytopo': ( lambda: MyTopo() ) }  
 
--- testbed/vid_prep.sh
+++ testbed/vid_prep.sh
@@ -3,6 +3,10 @@
 echo "Prepping bbb1.mp4"
 sudo ffmpeg -i bbb1.mp4 -hide_banner
 sudo ffmpeg -i bbb1.mp4 -t 00:02:00 bbb1_2m.mp4
-sudo x264 --output bbb_2400k.264 --fps 30 --preset slow --bitrate 2400 --vbvmaxrate 4800 --vbv-bufsize 9600 --min-keyint 96 --keyint 96 --scenecut 0 --noscenecut --pass 1 --video-filter resize:width=1280,height=720 bbb.mp4
-sudo MP4Box -add bbb_2400k.264 -fps 30 bbb_2400k.mp4
-sudo MP4Box -dash 4000 -frag 4000 -rap -segment-name segment_2400k_ bbb_2400k.mp4
+
+sudo x264 --output bbb_500k.264 --fps 30 --preset slow --bitrate 500 --vbv-maxrate 1000 --vbv-bufsize 2000 --min-keyint 96 --keyint 96 --scenecut 0 --no-scenecut --pass 1 --video-filter resize:width=1280,height=720 bbb1.mp4
+sudo x264 --output bbb_2400k.264 --fps 30 --preset slow --bitrate 2400 --vbv-maxrate 4800 --vbv-bufsize 9600 --min-keyint 96 --keyint 96 --scenecut 0 --no-scenecut --pass 1 --video-filter resize:width=1280,height=720 bbb1.mp4
+sudo x264 --output bbb_5000k.264 --fps 30 --preset slow --bitrate 1000 --vbv-maxrate 2000 --vbv-bufsize 4000 --min-keyint 96 --keyint 96 --scenecut 0 --no-scenecut --pass 1 --video-filter resize:width=1280,height=720 bbb1.mp4
+
+sudo MP4Box -add bbb_5000k.264 -cat bbb_2400k.264 -cat bbb_500k.264 -fps 30 bbb_all.mp4
+sudo MP4Box -dash 4000 -frag 4000 -rap -segment-name segment_2400k_ bbb_all.mp4
